e10fdd0dffb31c4c2d379b7237f9c33d3519a403
Imperior Hackers <58589595+imperiorhackers9934@users.noreply.github.com>
2025-07-23 11:14:42 +0530

    Fixed backend URL in api.js

--- webappy/frontend/src/services/api.js
+++ webappy/frontend/src/services/api.js
@@ -15,8 +15,8 @@ import axios from 'axios';
 // const API_URL = getApiUrl();
 
 
-// const API_URL = 'https://meetkats-backend.onrender.com';
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://meetkats-backend.onrender.com';
+//const API_URL = 'http://localhost:3000';
 console.log(`API URL: ${API_URL}`);
 
 // Axios instance
